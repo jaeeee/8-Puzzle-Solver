c4eded6c50368119c1cec396c99efbe971f66c05
Jae <daniel.kwong.h@gmail.com>
2021-02-18 18:47:10 -0800

    remove top comment

--- main.cpp
+++ main.cpp
@@ -9,17 +9,6 @@
 
 using namespace std;
 
-/*
-function general-search(problem, QUEUEING-FUNCTION)
-nodes = MAKE-QUEUE(MAKE-NODE(problem.INITIAL-STATE))
-loop do
-if EMPTY(nodes) then return "failure"
- node = REMOVE-FRONT(nodes)
-if problem.GOAL-TEST(node.STATE) succeeds then return node
- nodes = QUEUEING-FUNCTION(nodes, EXPAND(node, problem.OPERATORS))
-end
-*/
-
 struct puzzle_state
 {
     vector<vector<int>> game_state;
